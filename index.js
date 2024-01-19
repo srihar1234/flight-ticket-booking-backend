330a084cadd47e1bee296f9b5b922d235a1759aa
HARI <143261710+srihar1234@users.noreply.github.com>
2024-01-20 00:09:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const cors = require('cors');
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+
+app.use(express.json());
+app.use(cors({
+    origin:"*"
+}))
+
+app.post("/flights", async (req, res) => {
+    try {
+        const request = req.body;
+        const connection = await MongoClient.connect(url);
+        const db = connection.db("flight-ticket-booking");
+        const flight = await db.collection("flights").find({from:request.from,to:request.to}).toArray();
+        if (flight.length>0){
+            res.json(flight);
+        }
+        else{
+            res.send("no flights");
+        }
+        await connection.close();
+    }
+    catch (err) {
+        res.send(err);
+    }
+});
+
+app.listen(4001);
